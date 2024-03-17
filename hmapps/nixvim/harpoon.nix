a8808f4d5121e83ba7aa925ac3d472ed865d7e16
Anthony Abaray <122565774+Wick3d-git@users.noreply.github.com>
2024-03-17 11:13:50 -0400

    Add Harpoon and Undotree to Nixvim

---
+++ hmapps/nixvim/harpoon.nix
@@ -0,0 +1,17 @@
+{
+  programs.nixvim.plugins.harpoon = {
+    enable = true;
+    enableTelescope = true;
+    keymapsSilent = true;
+    keymaps = {
+      addFile = "<leader>a";
+      toggleQuickMenu = "<C-e>";
+      navFile = {
+        "1" = "<leader>n";
+        "2" = "<leader>e";
+        "3" = "<leader>i";
+        "4" = "<leader>o";
+      };
+    };
+  };
+}
