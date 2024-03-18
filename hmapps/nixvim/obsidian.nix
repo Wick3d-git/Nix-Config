b4d6789b68443e61fccea9916b9997d3559a03bd
Anthony Abaray <122565774+Wick3d-git@users.noreply.github.com>
2024-03-18 10:53:54 -0400

    Configure More Setup For Obsidian.nvim

--- hmapps/nixvim/obsidian.nix
+++ hmapps/nixvim/obsidian.nix
@@ -1,5 +1,24 @@
 {
   programs.nixvim.plugins.obsidian = {
     enable = true;
+    settings = {
+  completion = {
+    min_chars = 2;
+    nvim_cmp = true;
+  };
+  new_notes_location = "current_dir";
+  workspaces = [
+    {
+      name = "notes";
+      path = "~/Notes";
+    }
+    ];
+    templates = {
+      subdir = "Templates";
+      date_format = "%d-%m-%Y";
+      time_format = "%H:%M";
+      tags = "";
+  };
+  };
   };
 }
