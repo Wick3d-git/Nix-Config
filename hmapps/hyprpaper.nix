71726ab8de1346d373866ceb98e8a4169095ccfb
Anthony Abaray <122565774+Wick3d-git@users.noreply.github.com>
2024-10-25 19:14:21 -0400

    Intro Back Into Nix Pt. 2

--- hmapps/hyprpaper.nix
+++ hmapps/hyprpaper.nix
@@ -1,6 +1,6 @@
 {
   home.file.".config/hypr/hyprpaper.conf".text = 
-    "preload = /home/wick3d/NixOS-Config/Wallpapers/bg2.png
-    wallpaper = DP-3, /home/wick3d/NixOS-Config/Wallpapers/bg2.png
+    "preload = /home/wick3d/NixOS-Config/Downloads/Wallpapers/bg2.png
+    wallpaper = DP-3, /home/wick3d/NixOS-Config/Downloads/Wallpapers/bg2.png
     splash = false";
 }
